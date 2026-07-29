2083252330aa8a6931aa53ea5e3fc669eac54682
Vidyashree B Y <byvidyashree@gmail.com>
2026-07-29 09:58:42 +0530

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,37 @@
+body{
+    font-family: Arial, sans-serif;
+    background:#f2f2f2;
+}
 
+.container{
+    width:350px;
+    margin:80px auto;
+    background:white;
+    padding:20px;
+    border-radius:10px;
+    box-shadow:0 0 10px gray;
+    text-align:center;
+}
+
+input{
+    width:90%;
+    padding:10px;
+    margin:10px;
+}
+
+button{
+    background:green;
+    color:white;
+    padding:10px 20px;
+    border:none;
+    border-radius:5px;
+    cursor:pointer;
+}
+
+button:hover{
+    background:darkgreen;
+}
+
+h2{
+    color:blue;
+}
